65d33ddc0a762c415fec243859c6e1f7d4afe41a
guigui041201@gmail.com <guigui041201@gmail.com>
2022-04-30 00:10:02 -0300

    exemplo de função para criar objetos

--- src/TypeAnnotation/typeannotation.ts
+++ src/TypeAnnotation/typeannotation.ts
@@ -8,15 +8,34 @@ let student: boolean = true;
 let arrayOfNumber: number[] = [4, 4, 4, 2, 3, 3, 3];
 
 //OBJETOS
-let personObject: {namePerson: string, agePerson: number, marriedPerson?: boolean} = {
+let personObject: { namePerson: string, agePerson: number, marriedPerson?: boolean } = {
   namePerson: 'Paulo',
   agePerson: 21,
   marriedPerson: false
 }
 
 //FUNÇÕES
-function sum(x:number, y:number): number{
+function sum(x: number, y: number): number {
   return x + y;
 }
 
-const sum2: (x: number, y:number) => number = (x, y) => x + y;
+const sum2: (x: number, y: number) => number = (x, y) => x + y;
+
+export function createPerson(
+  //ABERTURA
+  firstName: string,
+  lastName: string,
+  phone?: number
+): {
+  //TYPE ANNOTATION
+  firstName: string;
+  lastName: string;
+  phone?: number
+} {
+  //CORPO DA FUNÇÃO
+  return {
+    firstName,
+    lastName,
+    phone
+  }
+}
