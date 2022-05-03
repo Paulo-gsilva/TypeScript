e07cbb476d3c8c95bb9a1f1a269c0805e2cbd249
guigui041201@gmail.com <guigui041201@gmail.com>
2022-05-03 10:35:15 -0300

    criando webpack para typescript

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************************************!*\
+  !*** ./src/section14/aula-webpack/index.ts ***!
+  \*********************************************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
